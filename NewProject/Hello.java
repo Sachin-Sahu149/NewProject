fb35b9a6dd19b71de66e4cbb5f829576fa1f4357
Sachin-Sahu149 <sachinsahu3222@gmail.com>
2024-08-08 07:04:26 +0530

    new File

---
+++ NewProject/Hello.java
@@ -0,0 +1,9 @@
+// package NewProject;
+ 
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Simple Java code ");
+        System.out.println("This is second line ");
+    }
+    
+}
